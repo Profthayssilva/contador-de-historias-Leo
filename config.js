5a8188abc42bee2f89a947feea4fc60bd3c3266f
Thays Silva <113531216+Profthayssilva@users.noreply.github.com>
2024-05-10 11:14:10 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCqo9s7nyYYqZ9_wEQmLiGywjsHrTTmSGI",
+  authDomain: "appcontadorleo.firebaseapp.com",
+  projectId: "appcontadorleo",
+  storageBucket: "appcontadorleo.appspot.com",
+  messagingSenderId: "609139457649",
+  appId: "1:609139457649:web:2c0734a2447edd9dd665d0"
+};
